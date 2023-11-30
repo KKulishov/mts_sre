29142156947d07603ff340b68869f52ec1a089c0
Кулишов Константин <kk@webpractik.ru>
2023-11-30 17:04:55 +0300

    test

--- Stress_testing/sre.py
+++ Stress_testing/sre.py
@@ -2,7 +2,7 @@ from locust import HttpUser, SequentialTaskSet, task, between
 import json
 import random
 class SreApp(SequentialTaskSet):
-    @task()
+    @task(30)
     def get_cities_and_fetch_id(self):
         # Шаг 1: Получаем JSON с /Cities
         response = self.client.get("/Cities")
@@ -29,7 +29,7 @@ class SreApp(SequentialTaskSet):
             else:
                 print(f"Failed to fetch data for city with id {selected_id}")
 
-    @task()
+    @task(20)
     def Forecast(self):
         with self.client.get("/Forecast", catch_response=True) as response:
             if response.status_code != 200:
@@ -39,9 +39,9 @@ class SreApp(SequentialTaskSet):
     def on_stop(self):
         self.user.stop()             
 
-"""
+
 class Random_change(SequentialTaskSet):
-    @task(10)
+    @task(5)
     def cities_id_change(self):
         # Шаг 1: Получаем текущие данные с /Forecast
         response = self.client.get("/Forecast")
@@ -64,19 +64,27 @@ class Random_change(SequentialTaskSet):
 
         # Шаг 3: Изменяем значение temperature и отправляем PUT-запрос
         if selected_forecast:
-            selected_forecast["temperature"] = random.uniform(1, 30)  # новая случайная температура
+            random_temperaure = random.uniform(1, 30)  # новая случайная температура
+
+            body = {
+                "id": int(selected_id),
+                "cityId": int(selected_id),
+                "dateTime": 11111,
+                "temperature": int(random_temperaure), # новая случайная температура,
+                "summary": "warm"
+            }
 
             # Отправляем PUT-запрос с обновленными данными
-            put_response = self.client.put(f"/Forecast/{selected_id}", json=selected_forecast)
+            put_response = self.client.put(f"/Forecast/{selected_id}", json=body)
 
             # Обработка результата в соответствии с вашими требованиями
             if put_response.status_code == 200:
-                print(f"Successfully updated temperature for forecast with id {selected_forecast['id']}")
+                print(f"Successfully updated temperature for forecast with id ")
             else:
-                print(f"Failed to update temperature for forecast with id {selected_forecast['id']}")
-"""
+                print(f"Failed to update temperature for forecast with id ")
+
 class WeatherForecast(SequentialTaskSet):
-    @task()
+    @task(10)
     def cities_route(self):
         endpoint = "/WeatherForecast"
         response = self.client.get(endpoint)
@@ -96,9 +104,10 @@ class WeatherForecast(SequentialTaskSet):
 
 
 class MyUser(HttpUser):
-    wait_time = between(1, 3)
+    wait_time = between(2, 6)
     # указания веса для распредление нагрузки
     tasks = {
-            SreApp: 8,
-            WeatherForecast: 2
+            SreApp: 10,
+            WeatherForecast: 2,
+            Random_change: 1
         }
